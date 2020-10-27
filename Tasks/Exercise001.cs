626c9b170c801f742c830228b21394d52311fd64
Brunastephane <73487831+Brunastephane@users.noreply.github.com>
2020-10-27 14:19:36 +0000

    Exercise 1 has finished

--- Tasks/Exercise001.cs
+++ Tasks/Exercise001.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 // In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. 
 // Your task will be to return the sum of the numbers that occur only once.
@@ -14,6 +15,22 @@ namespace TechReturners.Tasks
     {
         public static int Singles(List<int> source)
         {
+            //variable for sum the numbers 
+            int result = 0;
+
+            //This loop is getting every single number from source until the end of the list
+            foreach (int item in source)
+            {
+                //I`ve used linq expressions to avoid created other loop and other variables
+                //This If checks how quantity of times that number appears on the list and if is just once it will sum
+                if(source.Where(n => n == item).Count() == 1)
+                {
+                    //add the value on the variable result 
+                    result = result + item;
+                }
+            }
+            //return the sum
+            return result;
             throw new NotImplementedException();
         }
     }
